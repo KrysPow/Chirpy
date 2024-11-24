7703344128f1cc368dbd1b64834b1b52282d9ef2
KrysPow <pauer.christoph@gmail.com>
2024-11-24 17:38:08 +0100

    server

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
-	return
+	servMux := http.NewServeMux()
+	server := http.Server{
+		Handler: servMux,
+		Addr:    ":8080",
+	}
+
+	err := server.ListenAndServe()
+	fmt.Println(err)
 }
